0a97a04fc0a8837d2464858a6e8f308e0de59e1b
Ted <ted.pinkerton@mail.utoronto.ca>
2024-11-03 11:21:34 -0500

    Remove third person camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 use bevy_flycam::prelude::*;
-use bevy_third_person_camera::*;
 
 pub struct CameraPlugin;
 
@@ -19,11 +18,6 @@ fn spawn_camera(mut commands: Commands) {
     let camera = (
         Camera3dBundle {
             transform: Transform::from_xyz(-1.0, 1.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..Default::default()
-        },
-        ThirdPersonCamera {
-            zoom: Zoom::new(0.1, 50.0),
-            cursor_lock_key: KeyCode::Escape,
             ..Default::default()
         },
         FlyCam,
