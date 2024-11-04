7a2e8589acbbf33072641f9b59ae1ce2e0add72b
Ted <ted.pinkerton@mail.utoronto.ca>
2024-11-04 00:31:31 -0500

    Spawn camera in right spot

--- src/camera.rs
+++ src/camera.rs
@@ -17,7 +17,8 @@ impl Plugin for CameraPlugin {
 fn spawn_camera(mut commands: Commands) {
     let camera = (
         Camera3dBundle {
-            transform: Transform::from_xyz(-1.0, 1.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-0.3, 0.3, -0.3)
+                .looking_at(Vec3::new(0.0, 0.08, 0.0), Vec3::Y),
             ..Default::default()
         },
         FlyCam,
