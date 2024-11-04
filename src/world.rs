fad408c781ab7e50ac90a5455920025b0ff69e42
Ted <ted.pinkerton@mail.utoronto.ca>
2024-11-03 23:53:13 -0500

    Clean up world generation

--- src/world.rs
+++ src/world.rs
@@ -13,7 +13,7 @@ impl Plugin for WorldPlugin {
 fn spawn_lights(mut commands: Commands) {
     let directional_light = DirectionalLightBundle {
         directional_light: DirectionalLight {
-            illuminance: 5000.0,
+            illuminance: 1000.0,
             shadows_enabled: true,
             ..Default::default()
         },
@@ -31,20 +31,23 @@ fn spawn_floor(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    const FLOOR_RADIUS: f32 = 100.0;
+    const FLOOR_HEIGHT: f32 = 0.0001;
+
     commands
         .spawn(TransformBundle::default())
         .with_children(|parent| {
             parent.spawn(PbrBundle {
-                mesh: meshes.add(Mesh::from(Circle::new(100.0))),
+                mesh: meshes.add(Mesh::from(Cylinder::new(FLOOR_RADIUS, FLOOR_HEIGHT / 2.0))),
                 material: materials.add(StandardMaterial {
-                    base_color: color::Color::srgb(0.2 / 1.5, 1.0 / 1.5, 0.4 / 1.5),
+                    base_color: color::Color::srgb(0.0, 0.603922, 0.090196), // grass green
                     ..Default::default()
                 }),
-                transform: Transform::from_rotation(Quat::from_rotation_x(
-                    -std::f32::consts::FRAC_PI_2,
-                )),
                 ..Default::default()
             });
-            parent.spawn((RigidBody::Static, Collider::cylinder(100.0, 0.1)));
+            parent.spawn((
+                RigidBody::Static,
+                Collider::cylinder(FLOOR_RADIUS, FLOOR_HEIGHT / 2.0),
+            ));
         });
 }
